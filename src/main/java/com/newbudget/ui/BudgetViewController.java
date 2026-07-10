ba8593f7b3c9ceafd90a938533c0dbc4dbe38e33
jwbranch3i <jwbranch3@hotmail.com>
2026-07-10 13:58:02 -0400

    Enhance TreeTableCell styling; align number items to the right for improved readability

--- src/main/java/com/newbudget/ui/BudgetViewController.java
+++ src/main/java/com/newbudget/ui/BudgetViewController.java
@@ -498,6 +498,12 @@ public class BudgetViewController {
             }
         }) {
             @Override
+            public void updateItem(Number item, boolean empty) {
+                super.updateItem(item, empty);
+                setStyle("-fx-alignment: center-right;");
+            }
+
+            @Override
             public void startEdit() {
                 TreeTableView<BudgetTableRow> treeTable = getTreeTableView();
                 if (treeTable == null) {
